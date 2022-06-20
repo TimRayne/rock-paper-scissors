d0ecdfb0644bae228fa49d7f996aade1bb9c1146
TimRayne <timtinkler1329@live.com>
2022-06-19 23:52:26 -0700

    Create playRound function
    needed a function to compare playerSelection return and computerSelection return to determine a winner. playRound does this and returns a string containing a "winning" message, a "losing" message, or a "tie" message

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,3 +22,25 @@ function computerSelection(){
     return selection
 }
 
+// Play single round
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "rock"){
+        return "Tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose! Paper beats rock";
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Tie!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! Rock beats scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors beats paper";
+    } else if (playerSelection === "paper" && computerSelection === "paper"){
+        return "Tie!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper beats rock";
+    }
+}
