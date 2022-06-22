14675d6dee707ebc49da3fc70199813a1ab1783e
TimRayne <timtinkler1329@live.com>
2022-06-21 23:34:26 -0700

    Initialize playerScore and computerScore. Create game function. Make playRound function keep score
    Created variables playerScore and computerScore to keep score of game.
    game() function uses a for loop to call playRound() 5 times. Each time playRound is called either playerScore or computerScore is incremented by 1 for the winner. game() also compares playerScore to computerScore after the loop and console.log's a winner of the game.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,25 +22,45 @@ function computerSelection(){
     return selection
 }
 
+// Set scores to 0
+let playerScore = 0;
+let computerScore = 0;
+
 // Play single round
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "rock"){
-        return "Tie!";
+        return `Tie! \nPlayer ${playerScore} - Computer ${computerScore}`;
     } else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "You lose! Paper beats rock";
+        return `You lose! Paper beats rock \nPlayer ${playerScore} - Computer ${++computerScore}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You win! Rock beats scissors";
+        return `You win! Rock beats scissors \nPlayer ${++playerScore} - Computer ${computerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "scissors"){
-        return "Tie!";
+        return `Tie! Player ${playerScore} - Computer ${computerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return "You lose! Rock beats scissors";
+        return `You lose! Rock beats scissors \nPlayer ${playerScore} - Computer ${++computerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "You win! Scissors beats paper";
+        return `You win! Scissors beats paper \nPlayer ${++playerScore} - Computer ${computerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "paper"){
-        return "Tie!";
+        return `Tie! \nPlayer ${playerScore} - Computer ${computerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "scissors"){
-        return "You lose! Scissors beats paper";
+        return `You lose! Scissors beats paper \nPlayer ${playerScore} - Computer ${++computerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "You win! Paper beats rock";
+        return `You win! Paper beats rock \nPlayer ${++playerScore} - Computer ${computerScore}`;
     }
 }
+
+// Play 5 rounds
+function game(){
+    for (let i = 0; i < 5; i++){
+        console.log(playRound(playerSelection(), computerSelection()))
+    }
+    if (playerScore > computerScore){
+        console.log(`Congratulations! You won the game with a score of ${playerScore} to ${computerScore}`)
+    } else if (computerScore > playerScore){
+        console.log(`Ouch! You lost the game with a score of ${playerScore} to ${computerScore}`)
+    } else {
+        console.log(`Game Tied! Final scores were Player ${playerScore} - Computer ${computerScore}`)
+    }
+}
+
+game()
